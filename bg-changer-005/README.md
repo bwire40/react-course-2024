4ddeba8fa7568d66608059b3df4e5fd8e1e1e689
bwire40 <emmanuelbwire80@gmail.com>
2024-03-15 09:54:39 +0300

    done

--- bg-changer-005/README.md
+++ bg-changer-005/README.md
@@ -1,3 +1,5 @@
 # React + Vite
 
 Bg Changer app
+
+* the application uses states to change the background of the app using different buttons
--- bg-changer-005/src/App.jsx
+++ bg-changer-005/src/App.jsx
@@ -1,16 +1,49 @@
+import { useState } from "react";
+
 function App() {
+  // useState hook
+  const [color, setColor] = useState("olive");
+
+  // approach 1.
+  // const changeColor = (color) => {
+  //   setColor(color);
+  // };
+
+  // approach 2.
+  // using setcolor direclty to our buttons
   return (
-    <div className="w-full h-screen mx-auto duration-300 bg-black">
+    <div
+      className="w-full h-screen mx-auto duration-300 bg-black"
+      style={{ backgroundColor: color }}
+    >
       <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
         <div
           className="flex flex-wrap justify-center gap-3 shadow-lg bg-white
         px-3 py-2 rounded-3xl"
         >
-          <button className="w-24 outline-none py-1 rounded-full  bg-slate-600 text-white shadow-lg ">
-            test
+          <button
+            onClick={() => setColor("red")}
+            className="w-24 outline-none py-1 rounded-full  bg-slate-600 text-white shadow-lg "
+          >
+            Red
           </button>
-          <button className="w-24 outline-none py-1 rounded-full bg-slate-600 text-white shadow-lg ">
-            test
+          <button
+            onClick={() => setColor("green")}
+            className="w-24 outline-none py-1 rounded-full bg-slate-600 text-white shadow-lg "
+          >
+            Green
+          </button>
+          <button
+            onClick={() => setColor("white")}
+            className="w-24 outline-none py-1 rounded-full bg-slate-600 text-white shadow-lg "
+          >
+            White
+          </button>
+          <button
+            onClick={() => setColor("blue")}
+            className="w-24 outline-none py-1 rounded-full bg-slate-600 text-white shadow-lg "
+          >
+            Blue
           </button>
         </div>
       </div>
