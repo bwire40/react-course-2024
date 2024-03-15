c72cfb35998db1285a82a3cf2d6e1e54c72b0544
bwire40 <emmanuelbwire80@gmail.com>
2024-03-15 10:22:43 +0300

    UI done for the password generator

--- password-generator-006/src/App.jsx
+++ password-generator-006/src/App.jsx
@@ -1,5 +1,66 @@
+import { useState } from "react";
+
 function App() {
-  return <div>App</div>;
+  //our states
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+
+  //
+  return (
+    <div className="flex justify-center items-center flex-col py-10 bg-slate-800 w-full h-screen">
+      <h1 className="py-10 text-white text-2xl">Password Generator</h1>
+      <div className="flex">
+        <input
+          type="text"
+          className=" p-4 outline-none rounded-l-lg"
+          value={password}
+          placeholder="Password"
+          readOnly
+        />
+        <button className="bg-blue-600 p-4 text-white">Copy</button>
+      </div>
+      <div className="flex gap-x-2 text-sm">
+        <input
+          type="range"
+          id=""
+          min={6}
+          max={50}
+          value={length}
+          className="cursor-pointer"
+          onChange={(e) => setLength(e.target.value)}
+        />
+        <label className="text-white py-2" htmlFor="length">
+          Length: {length}
+        </label>
+      </div>
+      <div className="flex gap-x-2 text-sm">
+        <input
+          type="checkbox"
+          defaultChecked={numberAllowed}
+          id=""
+          className="cursor-pointer"
+          onChange={() => setNumberAllowed((prev) => !prev)}
+        />
+        <label className="text-white " htmlFor="number">
+          Numbers
+        </label>
+      </div>
+      <div className="flex gap-x-2 text-sm">
+        <input
+          type="checkbox"
+          defaultChecked={charAllowed}
+          id=""
+          className="cursor-pointer"
+          onChange={() => setCharAllowed((prev) => !prev)}
+        />
+        <label className="text-white " htmlFor="character">
+          Character
+        </label>
+      </div>
+    </div>
+  );
 }
 
 export default App;
