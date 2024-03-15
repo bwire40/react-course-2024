afadf36a632631979bce691fcca4d3dce0e476f8
bwire40 <emmanuelbwire80@gmail.com>
2024-03-15 08:04:49 +0300

    Simple intro to states

--- counter-003/README.md
+++ counter-003/README.md
@@ -1,3 +1,36 @@
-# React + Vite
+# React Full Course
 
-### variable injection using curly braces
+## Simple count application explaining the use of States
+
+- variable injection using curly braces
+
+### state
+
+- any change in state rerenders the webpage
+
+### Question
+
+ ***
+    const addValue = () => {
+    setCounter(counter + 1);
+    setCounter(counter + 1);
+    setCounter(counter + 1);
+    setCounter(counter + 1);
+  };
+
+- What do you think is the expected output for the above?
+
+### solution
+
+*** const addValue = () => {
+  
+    setCounter((prevCounter) => prevCounter + 1);
+    setCounter((prevCounter) => prevCounter + 1);
+    setCounter((prevCounter) => prevCounter + 1);
+    setCounter((prevCounter) => prevCounter + 1);
+    setCounter((prevCounter) => prevCounter + 1);
+  };
+
+- The Above count will increment by 5
+
+-
--- counter-003/src/App.jsx
+++ counter-003/src/App.jsx
@@ -1,17 +1,35 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  let counter = 0;
+  // use states
 
+  const [counter, setCounter] = useState(0);
+
+  // add value
   const addValue = () => {
-    counter = counter + 1;
-    console.log(counter);
+    // update the counter
+    setCounter((prevCounter) => prevCounter + 1);
+    setCounter((prevCounter) => prevCounter + 1);
+    setCounter((prevCounter) => prevCounter + 1);
+    setCounter((prevCounter) => prevCounter + 1);
+    setCounter((prevCounter) => prevCounter + 1);
+  };
+
+  // remove value
+  const removeValue = () => {
+    // update counter
+    if (counter > 0) {
+      setCounter(counter - 1);
+    }
   };
   return (
     <>
+      {/* use curly braces to inject varibales into jsx */}
       <h1>React Course with Manu {counter}</h1>
       <h2>Counter value: {counter}</h2>
-      <button onClick={addValue}>Add</button> <button>remove</button>
+      <button onClick={addValue}>Add</button>{" "}
+      <button onClick={removeValue}>remove</button>
       <p>Footer: {counter}</p>
     </>
   );
